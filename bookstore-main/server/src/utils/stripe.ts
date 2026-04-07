0c200af058dee8ee70c2d374c649ae0389773c0c
ashrafbilalmohaidat2 <ashrafmohaidat@gmail.com>
2026-04-07 21:28:28 +0300

    Update

--- bookstore-main/server/src/utils/stripe.ts
+++ bookstore-main/server/src/utils/stripe.ts
@@ -2,7 +2,7 @@ import Stripe from "stripe";
 import { serverConfig } from "../config";
 
 // Initialize Stripe with test key if no real key is provided
-const stripeKey = serverConfig.stripeSecretKey || "sk_test_4eC39HqLyjWDarhtT1ZdV7dc";
+const stripeKey = serverConfig.stripeSecretKey || "";
 
 export const stripe = new Stripe(stripeKey, {
   apiVersion: "2025-08-27.basil",
